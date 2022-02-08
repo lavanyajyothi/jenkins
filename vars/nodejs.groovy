5fa2e842a853e94add99c14ca18a006765f7cb62
lavanya <lavanya.bhadri>
2022-02-08 08:10:07 +0530

    pipeline code

---
+++ vars/nodejs.groovy
@@ -0,0 +1,32 @@
+def call(String COMPONENT) {
+  pipeline {
+    agent any
+
+    stages {
+
+      stage('compile') {
+        steps {
+          sh 'echo COMPONENT  = '${COMPONENT}
+        }
+      }
+
+//      stage('code quality') {
+//        steps {
+//          sh 'echo compile'
+//        }
+//      }
+
+//      stage('test cases') {
+//        steps {
+//          sh 'echo compile'
+//        }
+//      }
+    }
+  }
+}
+
+
+
+
+
+
